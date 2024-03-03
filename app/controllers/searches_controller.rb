0109df98ee03af30973e851aff584995ecdc2d35
makoto-niko <mmana19970613@gmail.com>
2024-03-03 05:52:54 +0000

    検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,4 @@
-class SeachesController < ApplicationController
+class SearchesController < ApplicationController
 before_action :authenticate_user!
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
    get "follower" => "relationship#followers", as: "followers"
   end 
   
-  get "/search", to: "search#searchs"
+  get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
